82794eeb9ee0293ea164671ff006c0da4572cd5d
Santiago Basulto <santiago.basulto@gmail.com>
2018-03-21 19:39:57 -0300

    Solution

--- todos.py
+++ todos.py
@@ -12,10 +12,30 @@ class TodoManager(object):
     def __init__(self, base_todos_path, create_dir=True):
         self.base_todos_path = base_todos_path
         self.path = Path(self.base_todos_path)
-        # continue here
+        if self.path.exists() and not self.path.is_dir():
+            raise ValueError('{} path is invalid'.format(base_todos_path))
+
+        if not self.path.exists():
+            if not create_dir:
+                raise ValueError("{} doesn't exist".format(base_todos_path))
+            self.path.mkdir(parents=True)
 
     def list(self, status=STATUS_ALL, category=CATEGORY_GENERAL):
-        pass
+        todos = {}
+        for todo_path in self.path.glob('*.json'):
+            with todo_path.open('r') as fp:
+                document = json.load(fp)
+                if 'category_name' not in document or 'todos' not in document:
+                    raise ValueError('Invalid JSON todo format.')
+
+                category_todos = []
+                for todo in document['todos']:
+                    if status == self.STATUS_ALL or todo['status'] == status:
+                        category_todos.append(todo)
+
+                todos[document['category_name']] = category_todos
+
+        return todos
 
     def new(self, task, category=CATEGORY_GENERAL, description=None,
             due_on=None):
@@ -29,4 +49,25 @@ class TodoManager(object):
             else:
                 raise ValueError('Invalid due_on type. Must be date or str')
 
-        # continue here
+        todo_file_name = '{}.json'.format(category)
+        path = self.path / todo_file_name
+
+        todos = {
+            'category_name': category.title(),
+            'todos': []
+        }
+        if path.exists():
+            with path.open('r') as fp:
+                todos = json.load(fp)
+
+        todo = {
+            'task': task,
+            'description': description,
+            'due_on': due_on,
+            'status': self.STATUS_PENDING
+        }
+
+        todos['todos'].append(todo)
+
+        with path.open('w') as fp:
+            todos = json.dump(todos, fp, indent=2)
